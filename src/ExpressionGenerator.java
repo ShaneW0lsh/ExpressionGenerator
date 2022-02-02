446bf3ad082473f8cfae5b035018663322c0ffdb
ShaneW0lsh <shanebetterfather@gmail.com>
2022-02-02 23:59:17 +0300

    added a basic workplace

--- src/ExpressionGenerator.java
+++ src/ExpressionGenerator.java
@@ -1,27 +1,26 @@
 import java.util.Random;
 
+// create a tree structure 
 class ExpressionGenerator { 
     Random rand = new Random();
 
     public static void main(String[] args) {
         Expression expr = new Expression();
 
-        for (int i = 0; i < 50; ++i) {
-            System.out.println(expr.evaluate());
-        }
+        evaluate(expr, 3);
     }
     
-    // public Expression evaluate(Expression expr, int depth) { 
-    //     if (depth <= 2) {
-    //         return expr;
-    //     }
-    //     int p = rand.nextInt(3);
-    //     if (p == 0) {
-    //         return evaluate(new MultiplyExpression(), depth-1);
-    //     } else if (p == 1) {
-    //         return evaluate(new AddExpression(), depth-1);
-    //     } else if (p == 2) {
-    //         return evaluate(new IntExpression(), depth-1);
-    //     }
-    // }
+    public String evaluate(Expression expr, int depth) { 
+        if (depth <= 2) {
+            return expr;
+        }
+        int p = rand.nextInt(3);
+        if (p == 0) {
+            return evaluate(new MultiplyExpression(), depth-1);
+        } else if (p == 1) {
+            return evaluate(new AddExpression(), depth-1);
+        } else if (p == 2) {
+            return evaluate(new IntExpression(), depth-1);
+        }
+    }
 }
