b8900611b2b6d6bbb7487b23b5050411a6046e4b
ShaneW0lsh <shanebetterfather@gmail.com>
2022-02-06 17:51:48 +0300

    tuned values

--- src/Expression.java
+++ src/Expression.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.lang.Math;
 
 // E 
 class Expression {
@@ -11,16 +12,16 @@ class Expression {
 
     }
 
-    public Expression(int depth) {
+    public Expression(int depth, int border) {
         if (depth <= 0) {
-            this.out = new IntExpression();
+            this.out = new IntExpression(border);
         } else {
             Random rand = new Random();
             int p = rand.nextInt(2);
             if (p == 1) {
-                this.out = new AddExpression(depth-1);
+                this.out = new AddExpression(depth-1, border);
             } else { 
-                this.out = new MultiplyExpression(depth-1);
+                this.out = new MultiplyExpression(depth-1, border);
             }
         }
         this.value = out.evaluate();
@@ -39,16 +40,16 @@ class Expression {
 // M
 class mExpression extends Expression{ 
 
-    public mExpression(int depth) {
+    public mExpression(int depth, int border) {
         if (depth <= 0) {
-            this.out = new IntExpression();
+            this.out = new IntExpression(border);
         } else {
             Random rand = new Random();
             int p = rand.nextInt(2);
             if (p == 1) {
-                this.out = new MultiplyExpression(depth-1);
+                this.out = new MultiplyExpression(depth-1, border);
             } else { 
-                this.out = new AddMExpression(depth-1);
+                this.out = new AddMExpression(depth-1, border);
             }
         }
         this.value = out.evaluate();
@@ -59,9 +60,9 @@ class mExpression extends Expression{
 class AddExpression extends Expression {
     private Expression left, right;
 
-    public AddExpression(int depth) { 
-        this.left = new Expression(depth-1);
-        this.right = new Expression(depth-1);
+    public AddExpression(int depth, int border) { 
+        this.left = new Expression(depth-1, border/2);
+        this.right = new Expression(depth-1, border/2);
 
         this.value = left.evaluate() + right.evaluate();
         this.inString = String.format("%s + %s", left.toString(), right.toString());    
@@ -71,9 +72,9 @@ class AddExpression extends Expression {
 class AddMExpression extends Expression {
     private Expression left, right;
 
-    public AddMExpression(int depth) { 
-        this.left = new Expression(depth-1);
-        this.right = new Expression(depth-1);
+    public AddMExpression(int depth, int border) { 
+        this.left = new Expression(depth-1, border/2);
+        this.right = new Expression(depth-1, border/2);
 
         this.value = left.evaluate() + right.evaluate();
         this.inString = String.format("(%s + %s)", left.toString(), right.toString());    
@@ -83,9 +84,9 @@ class AddMExpression extends Expression {
 class MultiplyExpression extends Expression {
     private Expression left, right;
 
-    public MultiplyExpression(int depth) { 
-        this.left = new mExpression(depth-1);
-        this.right = new mExpression(depth-1);
+    public MultiplyExpression(int depth, int border) { 
+        this.left = new mExpression(depth-1, (int)Math.sqrt(border));
+        this.right = new mExpression(depth-1, (int)Math.sqrt(border));
 
         this.value = left.evaluate() * right.evaluate();
         this.inString = String.format("%s * %s", left.toString(), right.toString());    
@@ -95,9 +96,9 @@ class MultiplyExpression extends Expression {
 
 class IntExpression extends Expression {
 
-    public IntExpression() {
+    public IntExpression(int border) {
         Random rand = new Random();
-        this.value = rand.nextInt(101);
+        this.value = rand.nextInt(border) + 1;
         this.inString = Integer.toString(this.value);
     }
 }
--- src/ExpressionGenerator.java
+++ src/ExpressionGenerator.java
@@ -2,7 +2,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 // TODO: 
-// 2. Answers are too big 
+// 3. Integrate this into the main project 
+// 4. Organize packages in the main project, call this one Expression, and the main one
 
 
 class ExpressionGenerator { 
@@ -14,11 +15,15 @@ class ExpressionGenerator {
 
         int depth = sc.nextInt();
         int n = sc.nextInt();
-        int border = 100;
-
+        int border = 5000;
+        
+        int biggest = 0;
         for (int i = 0; i < n; ++i) { 
             Expression expr = new Expression(depth, border);
+            biggest = Math.max(biggest, expr.evaluate());
             System.out.println(String.format("%s = %d", expr.toString(), expr.evaluate()));
         }
+
+        System.out.println(biggest);
     }
 }
